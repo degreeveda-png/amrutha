f91838e6532f6cc7b0a0c7afeedff29b9e53aed6
degreeveda-png <degreeveda@gmail.com>
2026-01-22 00:35:00 -0500

    Add greeting header to App component

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>helllooo students!!!!!!!!!</h1>
+      <h2>good morning!!!!!!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
